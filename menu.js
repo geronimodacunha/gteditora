6474b7bc98a395cbe29c97d918e6ea7e6e663705
https://github.com/geronimodacunha/ <jeronimodacunha163@gmail.com>
2023-08-18 02:17:37 +0100

    inovei

---
+++ menu.js
@@ -0,0 +1,19 @@
+var menu = document.getElementById("menu")
+var topo = document.getElementById("topo")
+
+menu.addEventListener('click', function(){
+    if(topo.style.display == "block"){
+        topo.style.display = "none"
+    }
+    else{
+        topo.style.display = "block"
+    }
+})
+
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+  const opacity = Math.min(1, scrollPosition / 300);
+  topo.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+ topo.style.height="60px"
+});
+
