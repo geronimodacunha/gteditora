df129a12d2f788273997a3de35f2e20b32596f99
https://github.com/geronimodacunha/ <jeronimodacunha163@gmail.com>
2023-08-18 23:58:30 +0100

    menu responsivo

--- Js/menu.js
+++ Js/menu.js
@@ -10,10 +10,9 @@ menu.addEventListener('click', function(){
     }
 })
 
-window.addEventListener('scroll', () => {
-  const scrollPosition = window.scrollY;
-  const opacity = Math.min(1, scrollPosition / 300);
-  topo.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
- topo.style.height="60px"
-});
 
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    const opacity = Math.min(1, scrollPosition / 300); // Adjust the division value for sensitivity
+    topo.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+});
